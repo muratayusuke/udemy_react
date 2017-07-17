5fa2cfdf9d7b29a499ec704ec5ab97f6b9780e1e
muratayusuke <info@muratayusuke.com>
2017-07-18 01:43:17 +0900

    Hello ES2015

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var publidDir = __dirname + '/public';
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: publidDir,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: publidDir
+  }
+};
