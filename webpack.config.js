51f12aa1115fb3633c39e0dfaeefcbf9250d1851
muratayusuke <info@muratayusuke.com>
2017-07-19 00:35:42 +0900

    Introduce scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,57 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const publidDir = path.join(__dirname, '/public');
-module.exports = {
-  entry: [
-    './src/index.js',
-  ],
-  output: {
-    path: publidDir,
-    publicPath: '/',
-    filename: 'bundle.js',
+module.exports = [
+  {
+    entry: [
+      './src/index.js',
+    ],
+    output: {
+      path: publidDir,
+      publicPath: '/',
+      filename: 'bundle.js',
+    },
+    module: {
+      loaders: [{
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      }],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+      historyApiFallback: true,
+      contentBase: publidDir,
+    },
   },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015'],
-      },
-    }],
+  {
+    entry: {
+      style: './stylesheets/index.scss',
+    },
+    output: {
+      path: publidDir,
+      publicPath: '/',
+      filename: 'bundle.css',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }),
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }),
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin('bundle.css'),
+    ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: publidDir,
-  },
-};
+];
