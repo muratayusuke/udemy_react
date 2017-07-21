339d841726b622241f75946a2ee7a866950d170f
muratayusuke <info@muratayusuke.com>
2017-07-21 22:59:36 +0900

    Introduce state

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,11 +3,28 @@ import React, { Component } from 'react';
 import Greeting from './greeting';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'John',
+    };
+  }
+
+  handleMouseOver() {
+    this.setState({ name: 'Bob' });
+  }
+
+  handleMouseOut() {
+    this.setState({ name: 'Mike' });
+  }
+
   render() {
     return (
-      <div>
-        <Greeting name="Bob" />
-        <Greeting name="John" />
+      <div
+        onMouseOver={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+      >
+        <Greeting name={this.state.name} />
       </div>
     );
   }
