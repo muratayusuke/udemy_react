4999f970a49f3480152cef8a35ed6e2302359a60
muratayusuke <info@muratayusuke.com>
2017-08-17 01:11:05 +0900

    Install react-router and add about page

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <div className="about">
+    <h1 className="app-title">このサイトについて</h1>
+  </div>
+);
+
+export default AboutPage;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom';
 
 import SearchPage from './SearchPage';
+import AboutPage from './AboutPage';
 
 const App = () => (
-  <div className="app">
-    <ul className="left-navi">
-      <li><a href="/">ホテル検索</a></li>
-      <li><a href="/about">About</a></li>
-    </ul>
-    <SearchPage />
-  </div>
+  <Router>
+    <div className="app">
+      <ul className="left-navi">
+        <li><Link to="/">ホテル検索</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={SearchPage} />
+        <Route exact path="/about" component={AboutPage} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
